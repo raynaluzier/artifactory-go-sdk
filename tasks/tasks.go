6134ce7eccedcd410733cd5710fd184e24dda417
Rayna Luzier <rluzier@riverpointtechnology.com>
2024-12-12 10:49:10 -0700

    reorg tasks, rename file, fix TeardownTest func

--- tasks/tasks.go
+++ tasks/tasks.go
@@ -2,6 +2,7 @@ package tasks
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/raynaluzier/artifactory-go-sdk/common"
 	"github.com/raynaluzier/artifactory-go-sdk/operations"
@@ -61,4 +62,55 @@ func GetImageDetails(serverApi, token, logLevel, artifName, ext string, kvProps 
 	}
 	
 	return artifactUri, artifactName, createDate, downloadUri
+}
+
+// Must have Artifactory instance licensed at Pro or higher, access to create/remove repos and artifacts
+func SetupTest(serverApi, token, testRepoName, configFilePath, testArtifact, fileSuffix string) (string, error) {
+	util.ServerApi = serverApi
+	util.Token	   = token
+
+	testRepo, err  := common.CreateTestRepo(testRepoName, configFilePath)
+	if err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+
+	downloadUri, err := operations.UploadFile(testArtifact, testRepo, fileSuffix)
+	if err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+
+	artifactUri := common.SetArtifUriFromDownloadUri(downloadUri)
+
+	return artifactUri, nil
+}
+
+func TeardownTest(serverApi, token, testRepoName, artifactUri string) (string) {
+	util.ServerApi = serverApi
+	util.Token	   = token
+
+	// Delete test artifact
+	statusCodeArtif, err := operations.DeleteArtifact(artifactUri)
+	if statusCodeArtif == "204" {
+		fmt.Println("Deletion of test artifact completed successfully.")
+	} else {
+		fmt.Println("Unable to delete test artifact - ", err)
+	}
+
+	// Delete test repo
+	statusCodeRepo, err := common.DeleteTestRepo(testRepoName)
+	if statusCodeRepo == "204" {
+		fmt.Println("Deletion of test repo completed successfully.")
+	} else {
+		fmt.Println("Unable to delete test repo - ", err)
+	}
+
+	if statusCodeArtif == "204" && statusCodeRepo == "204" {
+		statusCode := "204"
+		return statusCode
+	} else {
+		statusCode := "404"
+		return statusCode
+	}
 }
